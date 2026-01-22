9da19a313ee354525552229bb79cc2c5e8b9f25b
jiangzhongtian <jiangzhongtian@example.com>
2026-01-22 17:40:39 +0800

    Update base path for new repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/annual-report-2026/',
+      base: '/ai-real-estate-annual-report/',
       server: {
         port: 3000,
         host: '0.0.0.0',
